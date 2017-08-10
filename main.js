a313cb0773edec2b93da3be8a44344e6bee03dfa
Buffy Feinstein <buffyfeinstein@gmail.com>
2017-08-10 15:31:42 -0500

    js audio working

--- main.js
+++ main.js
@@ -8,37 +8,40 @@
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-
-
-// var displayArea = document.querySelector(".images");
+const URL = "https://itunes.apple.com/search?term=";
 var input = document.querySelector("#input");
 var submit = document.querySelector("#submit");
 var resultsDisplay = document.querySelector(".mainContent");
-
+var audio = document.querySelector("audio");
 
 submit.addEventListener("click", function (e) {
-
-
-  // if (e.keyCode === 13) {
-  let url = "https://itunes.apple.com/search?term=";
-  url += input.value;
-  console.log(url);
-
   e.preventDefault();
-
-  axios.get(url)
+  let userInput = input.value;
+  axios.get(URL + userInput)
     .then(function (response) {
       let results = response.data.results;
-      resultsDisplay.innerHTML = " ";
+      populateHTML(results)
+    });
+});
 
-      for (var i = 0; i < results.length; i++) {
-        let data = results[i];
-        let artistName = results[i].artistName;
-        let albumArtwork = results[i].artworkUrl100;
-        let trackName = results[i].trackName;
+resultsDisplay.addEventListener("click", function (e) {
+
+  audio.setAttribute("src", e.target.value);
+  // audio.src = e.target.value;\
+})
 
 
-        resultsDisplay.innerHTML += `
+function populateHTML(res) {
+  resultsDisplay.innerHTML = " ";
+
+  for (var i = 0; i < res.length; i++) {
+    let data = res[i];
+    let artistName = res[i].artistName;
+    let albumArtwork = res[i].artworkUrl100;
+    let trackName = res[i].trackName;
+    let preview = res[i].previewUrl;
+
+    resultsDisplay.innerHTML += `
 
           <div class="container-results">
             <div class="image-parent">
@@ -48,40 +51,9 @@ submit.addEventListener("click", function (e) {
             <div class="content-parent">
               <p>${artistName}</p>
               <p>${trackName}</p>
+              <button type="button" value=${preview}>Play</button>
             </div>
           </div>
-          `
-      }
-
-    });
-
-
-
-
-});
-
-  // fetch(`https://itunes.apple.com/search?term==${input.value}`)
-  //   .then(function (response) {
-  //     response.json().then(function (data) {
-  //       let results = data.results;
-  //       resultsDisplay.innerHTML = " ";
-
-      // })
-    // });
-
-// return data.json().then(function(data) {
-
-
-
-      // <a href ="${href}" target="_blank"><img src="https://s3-ap-southeast-1.amazonaws.com/assets.limetray.com/assets/user_images/menus/original/1442837463_Imgggg.jpg"><div class="caption">${title}</div></a>`;
-
-
-    // });
-// }
-// }
-
-// search.addEventListener("keypress", ourCallback);
-// search.addEventListener("click", function () {
-//   displayArea.innerHTML = "";
-//   search.value = "";
-
+         `
+  }
+}
